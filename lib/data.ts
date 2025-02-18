f42603b6727a3c1dee6027fce2348c164c4c302e
Dat Nguyen <datnguyen01122003@gmail.com>
2025-02-19 00:08:06 +0700

    fix image name bug

--- lib/data.ts
+++ lib/data.ts
@@ -5,7 +5,7 @@ import { LuGraduationCap } from "react-icons/lu";
 // import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
-import learnEnglishImg from "@/public/learn-english.png";
+import learnEnglishImg from "@/public/learnEnglish.png";
 
 export const links = [
   {
